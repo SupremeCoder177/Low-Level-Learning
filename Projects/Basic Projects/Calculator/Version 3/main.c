4801cc79987f670f82bb3c2a7dbfcf8956a1ca33
unknown <onlyforworkplsremember@gmail.com>
2025-08-02 03:53:19 +0530

    Made Calculator Version 3 with user input error handling

--- Projects/Basic Projects/Calculator/Version 3/main.c
+++ Projects/Basic Projects/Calculator/Version 3/main.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <string.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <ctype.h>
 #include <stdbool.h>
 
 void print(const char* string){
@@ -10,47 +13,135 @@ void print(const char* string){
     putchar('\n');
 }
 
-bool checkInInt(int* nums, int length, int num){
-    for(int i = 0; i < length; i++){
-        if(nums[i] == num){
-            return true;
+int get_int(const char* prompt){
+    char buf[100];
+    while(1){
+        printf("%s\n", prompt);
+        if(fgets(buf, sizeof(buf), stdin) == NULL){
+            fprintf(stderr, "Please only enter valid input.\n");
+            continue;
         }
+
+        buf[strcspn(buf, "\n")] = '\0';
+
+        if(buf[0] == '\0'){
+            fprintf(stderr, "Please enter something.\n");
+            continue;
+        }
+        errno = 0;
+        char* endptr;
+        long val = strtol(buf, &endptr, 10);
+        if(buf[0] == *endptr){
+            printf("No valid intergers found.\n");
+            continue;
+        }
+        if(*endptr != '\0'){
+            printf("Other characters than integers entered !!\n");
+            continue;
+        }
+        if(errno == ERANGE){
+            printf("Ineger out of range. Try again.\n");
+            continue;
+        }
+
+        return (int)val;
     }
-    return false; 
+}
+
+float get_float(const char* prompt){
+    char buf[100];
+    while(1){
+        printf("%s", prompt);
+        if(fgets(buf, sizeof(buf), stdin) == NULL){
+            fprintf(stderr, "Invalid input !!\n");
+            continue;
+        }
+
+        buf[strcspn(buf, "\n")] = '\0';
+        if(buf[0] == '\0'){
+            printf("Please enter something !\n");
+            continue;
+        }
+        errno = 0;
+        char* temp;
+        double val = strtod(buf, &temp);
+        if(buf[0] == *temp){
+            printf("Invalid input !\n");
+            continue;
+        }
+        if(*temp != '\0'){
+            printf("Other characters than numbers entered !\n");
+            continue;
+        }
+        if(errno == ERANGE){
+            printf("Entered value out of range !\n");
+            continue;
+        }
+        return val;
+    }
+}
+
+void print_commands(){
+    print("0 = Exit");
+    print("1 = Add");
+    print("2 = Subtract");
+    print("3 = Multiply");
+    print("4 = Divide");
+    print("5 = See current Value");
+    print("6 = Reset Value");
 }
 
 int main(int argc, char* argv[]){
-    
-    double num = 0;
-    char* name[100];
-    int valid_inputs[] = {1, 2, 3, 4, 0};
-    int user_input = -1;
-    print("Welcome again to my calculator CLI app.");
-    print("Care to tell me your name, this is our third time meeting after all:");
-    scanf("%100s", name);
-    printf("Hello there %s\n", name);
 
-    do{
-        printf("Current Number : %f\n", num);
-        print("Well here are the commands:");
-        print("0 = Exit");
-        print("1 = Add");
-        print("2 = Sub");
-        print("3 = Multiply");
-        print("4 = Divide");
-        scanf("%d", user_input);
-        if(!checkInInt(valid_inputs, sizeof(valid_inputs) / sizeof(valid_inputs[0]), user_input)){
-            print("Come on bro just enter the valid commands.");
-            break;
-        }else{
-            switch (user_input)
-            {
-            case 0:
-                break;
-            default:
-                break;
-            }
+    print("Welcome to VERSION 3.0 Calculator CLI version");
+    print("I won't bore you with the commands like usual");
+    int user_input;
+    double num = 0;
+    float temp;
+    bool exit = false;
+    while(!exit){
+        print("Enter 10 to see commands.");
+        user_input = get_int("Enter:");
+        if(user_input < 0){
+            fprintf(stderr, "Commands cannot be negative values, enter 10 to see command list.");
+            continue;
         }
-    }while(user_input != 0);
-    return 0;
+        if(user_input == 0){
+            print("Ok bye!...");
+            exit = true;
+            continue;
+        }
+        if(user_input == 5){
+            printf("%f\n", num);
+            continue;
+        }
+        if(user_input == 6){
+            print("Value has been reset.");
+            num = 0;
+            continue;
+        }
+        if(user_input == 10){
+            print_commands();
+            continue;
+        }
+        if(user_input >= 1 && user_input <= 4){
+            temp = get_float("Enter:");
+        }
+        switch(user_input){
+            case 1:
+                num += temp;
+                break;
+            case 2:
+                num -= temp;
+                break;
+            case 3:
+                num *= temp;
+                break;
+            case 4:
+                num /= temp;
+                break;
+        }
+    }
+       
+   return 0;
 }
